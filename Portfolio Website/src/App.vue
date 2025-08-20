4a23ec257ffa77097eb085067934faf9b04d8dae
maks <nazwiskomaks29@gmail.com>
2025-08-20 19:20:44 +0200

    base 1.2

--- Portfolio Website/src/App.vue
+++ Portfolio Website/src/App.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import HomePage from "./components/pages/Home.vue";
+</script>
 
-<template></template>
+<template>
+  <HomePage />
+</template>
 
 <style scoped></style>
--- Portfolio Website/src/components/pages/Home.vue
+++ Portfolio Website/src/components/pages/Home.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <h1>Klemensowicz</h1>
 </template>
 
 <style lang="scss" scoped></style>
