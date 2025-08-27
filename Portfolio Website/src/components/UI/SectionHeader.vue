426b10ffb25a81dea0b199ba112ed4949e8a3bba
maks <nazwiskomaks29@gmail.com>
2025-08-27 21:50:16 +0200

    section header 1.0

--- Portfolio Website/src/components/UI/SectionHeader.vue
+++ Portfolio Website/src/components/UI/SectionHeader.vue
@@ -1,17 +1,382 @@
 <script setup>
 const props = defineProps({
   title: String,
+  number: Number,
 });
 </script>
 
 <template>
   <div class="container">
+    <div class="bg-section">
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="bg-section bottom">
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+      <div class="wrapper">
+        <div class="line"></div>
+      </div>
+    </div>
+    <div class="carousel">
+      <div class="wrapper">
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="dim">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="soft">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="faint">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="normal">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bold">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bright">{{ number }}</strong>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="soft">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="faint">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="strong">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="dim">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="normal">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bright">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+      </div>
+    </div>
     <h1>{{ title }}</h1>
+    <div class="carousel no2">
+      <div class="wrapper">
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bold">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="faint">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bright">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="dim">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="strong">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="num">
+          <strong class="soft">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="normal">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="faint">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bold">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="bright">{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong>{{ number }}</strong>
+        </div>
+        <div class="num">
+          <strong class="dim">{{ number }}</strong>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-h1 {
-  color: white;
+@use "../../styles/base.scss" as *;
+
+.container {
+  width: 100%;
+  height: 250px;
+  background: $white;
+  position: relative;
+  display: flex;
+
+  h1 {
+    font-family: $primary-font;
+    font-size: $XL-size-font;
+    font-weight: 400;
+    width: 95%;
+    @include FlexCenter(row);
+    letter-spacing: 17px;
+  }
+
+  .carousel {
+    width: 2.5%;
+    overflow: hidden;
+    @include FlexCenter(column);
+
+    .wrapper {
+      animation: anim 7s linear infinite;
+
+      &:nth-of-type(2) {
+        animation-delay: 7s;
+      }
+
+      .num {
+        font-family: $secondary-font;
+        font-size: $M-size-font;
+      }
+    }
+
+    &.no2 {
+      .wrapper {
+        animation-direction: reverse;
+      }
+    }
+  }
+
+  .bg-section {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    position: absolute;
+    z-index: 1;
+
+    &.bottom {
+      .wrapper {
+        align-items: end;
+      }
+    }
+
+    .wrapper {
+      height: 100%;
+      display: flex;
+      gap: 7px;
+
+      .line {
+        height: 40px;
+        width: 1px;
+        background: $black;
+        opacity: 17.5%;
+      }
+    }
+  }
+}
+
+@keyframes anim {
+  to {
+    transform: translateY(-50%);
+  }
 }
 </style>
--- Portfolio Website/src/components/sections/About.vue
+++ Portfolio Website/src/components/sections/About.vue
@@ -4,13 +4,14 @@ import SectionHeader from "../UI/SectionHeader.vue";
 
 <template>
   <section id="about">
-    <SectionHeader title="About"></SectionHeader>
+    <SectionHeader title="About me" number="1"></SectionHeader>
   </section>
 </template>
 
 <style lang="scss" scoped>
 #about {
   width: calc(100svw - 29px);
-  margin-left: 9px;
+  margin: 500px 0px 150px 7px;
+  padding: 0px 16.5%;
 }
 </style>
