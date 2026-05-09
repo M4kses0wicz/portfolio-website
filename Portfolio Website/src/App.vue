d930c0fcd6f126ff56b19c347ca40be2c7db4c5d
maks <nazwiskomaks29@gmail.com>
2026-05-09 17:44:53 +0200

    smooth scrolling

--- Portfolio Website/src/App.vue
+++ Portfolio Website/src/App.vue
@@ -2,6 +2,20 @@
 import Home from "./components/pages/Home.vue";
 import LoadingScreen from "./components/UI/LoadingScreen.vue";
 import InfoCard from "./components/UI/InfoCard.vue";
+
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.25,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 </script>
 
 <template>
